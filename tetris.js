e7b485bf4698c7ad1f051f9bbcebe4d243f26393
Sonseongoh <nobleser@naver.com>
2023-11-13 13:01:17 +0900

    tree 블럭 생성

--- tetris.js
+++ tetris.js
@@ -5,7 +5,6 @@ const playground = document.querySelector(".playground > ul")
 const GAME_ROWS =20;
 const GAME_COLS =10;
 
-init() //게임판 실행
 
 //variables
 let score =0;
@@ -14,26 +13,32 @@ let downInterval;
 let tempMovingItem; //movingIem을 실행하기전에 잠깐 담아두는 용도
 
 const BLOCKS ={
-    tree:[      // ㅗ 모양
-        [],
-        [],
-        [],
-        [],
-    ]
+    tree:[      // ㅗ 모양  , 돌릴떄마다 다른모양 총 4가지 배열
+    [[2,1],[0,1],[1,0],[1,1]], //총4개칸으로 구성된  1개의 블럭 
+    [],
+    [],
+    [],
+]
 }
 
 const movingItem={  //다음블럭의 타입과 좌표정보등을 가짐
-    type:"",
+    type:"tree",   //블럭의 형태를 가져옴
     direction:0,   //화살표 윗방향키를 눌렀을때 좌우로 돌리는 역할
     top:0,
     left:0,
 }
 
+
+init() //게임판 실행
+
 //functions
-function init(){
+function init(){  
+    tempMovingItem ={...movingItem};   //spread 연산자로 넣는 이유는 movingItem 의 값만 
+                                        //넣어줌으로써 movingItem이 변경되어도 tempmovingItem은 변경되지 않도록
     for(let i =0; i< GAME_ROWS; i++){
         prependNewLine()
     }
+    renderBlocks()
 }
 
 
@@ -47,4 +52,15 @@ function prependNewLine(){
     }
     li.prepend(ul)
     playground.prepend(li)
+}
+
+function renderBlocks(){    
+    const{type,direction,top,left} = tempMovingItem; //tempMovingItem 안에 들어있는 각각의 프로퍼티들을 변수로 사용
+
+    BLOCKS[type][direction].forEach(block =>{  //type은 tree와 같은 블럭의 형태
+        const x= block[0]
+        const y= block[1]
+        const target = playground.childNodes[y].childNodes[0].childNodes[x] //li
+        target.classList.add(type)  // li에 tree라는 클래스를 부여  , css 에서도 .class 만들어주기
+    })
 }
